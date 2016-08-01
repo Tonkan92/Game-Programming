7d14d5df22f275c394286e9a1cc6bff3c0ecbe98
Tony <Tony@Tony-PC>
2016-08-01 13:12:06 +0200

    episode 7

--- src/com/tonkan/rain/Game.java
+++ src/com/tonkan/rain/Game.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -14,24 +16,27 @@ public class Game extends Canvas implements Runnable {
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
-		Dimension size = new Dimension(width*scale, height*scale);
+		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
 		frame = new JFrame();
 	}
-	
+
 	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -40,32 +45,32 @@ public class Game extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		while (running) {
 			update();
 			render();
 		}
 	}
-	
+
 	public void update() {
-		
+
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
 		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g.dispose();
 		bs.show();
 	}
-	
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
@@ -75,10 +80,8 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
-	
-	
-	
+
 }
